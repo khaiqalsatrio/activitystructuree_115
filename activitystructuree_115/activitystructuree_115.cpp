4d4ec1dd5228a5ad3e63a0f72c8e9576a954d218
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-04-03 11:49:51 +0700

    latihan.1

--- activitystructuree_115/activitystructuree_115.cpp
+++ activitystructuree_115/activitystructuree_115.cpp
@@ -1,20 +1,42 @@
-// activitystructuree_115.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+struct mahasiswa
 {
-    std::cout << "Hello World!\n";
+	string NIM;
+	string nama;
+
+	string alamat;
+	int umur;
+};
+
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20210140019";
+	mhs1.nama = "Ardiyan Mulya";
+	mhs1.alamat = "wonosobo";
+	mhs1.umur = 20;
+
+	cout << "Masukan NIM :";
+	cin >> mhs2.NIM;
+	cout << "masukan nama :";
+	cin >> mhs2.nama;
+	cout << "masukan alamat :";
+	cin >> mhs2.alamat;
+	cout << "masukan unsur : ";
+	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUnsur = " << mhs1.umur;
+
+	cout << "\n\nNim = " << mhs2.NIM;
+	cout << "\nNama = " << mhs2.nama;
+	cout << "\nAlamat = " << mhs2.alamat;
+	cout << "\nUmur = " << mhs2.umur;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
